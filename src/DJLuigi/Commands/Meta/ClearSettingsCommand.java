24302666f93021c5cd0a0c785f27bfd1b42542c8
Michael Nix <michael.g.nix@gmail.com>
2021-10-04 15:04:14 -0500

    Added a confirmation menu

--- src/DJLuigi/Commands/Meta/ClearSettingsCommand.java
+++ src/DJLuigi/Commands/Meta/ClearSettingsCommand.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import DJLuigi.Commands.Command;
 import DJLuigi.Commands.CommandCategory;
 import DJLuigi.Commands.CommandData;
+import DJLuigi.Interaction.ReactionConfirmation;
 import DJLuigi.Server.Server;
 import DJLuigi.Server.ServerSettings;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -24,6 +25,13 @@ public class ClearSettingsCommand implements Command
 	@Override
 	public void executeCommand(Server S, ArrayList<String> Parameters, MessageReceivedEvent event) 
 	{
+		new ReactionConfirmation("Do you really want to clear all of the settings? There's no going back...", event,
+			() -> clearSettings(S),
+			() -> S.SendMessage("Clear Settings Aborted"));
+	}
+	
+	public void clearSettings(Server S)
+	{
 		S.data.settings = new ServerSettings();
 		
 		try {
@@ -33,7 +41,6 @@ public class ClearSettingsCommand implements Command
 			S.SendMessage("Something went wrong!");
 			e.printStackTrace();
 		}
-		
 	}
 
 }
