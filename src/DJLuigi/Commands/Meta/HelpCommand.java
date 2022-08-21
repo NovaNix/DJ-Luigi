e706d12a482a742c76cbad6688039087fc8177b0
Michael Nix <michael.g.nix@gmail.com>
2022-08-20 20:51:03 -0500

    Added more help command aliases

--- src/DJLuigi/Commands/Meta/HelpCommand.java
+++ src/DJLuigi/Commands/Meta/HelpCommand.java
@@ -14,7 +14,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 (
 	command = "help", 
 	description = "Lists the available commands",
-	aliases = {"?", "h"},
+	aliases = {"?", "h", "commands", "hep"},
 	category = CommandCategory.Other
 )
 public class HelpCommand implements Command
