671951581ff440d34cf7e6b56bac69066b3db7f2
Michael Nix <michael.g.nix@gmail.com>
2022-08-22 19:43:07 -0500

    Added parameters

--- src/DJLuigi/Commands/Audio/RemoveFromQueueCommand.java
+++ src/DJLuigi/Commands/Audio/RemoveFromQueueCommand.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 import DJLuigi.Commands.Command;
 import DJLuigi.Commands.CommandCategory;
 import DJLuigi.Commands.CommandData;
+import DJLuigi.Commands.Parameter;
 import DJLuigi.Server.Server;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 
 @CommandData
 (
 	command = "remove", 
 	description = "Removes a song from the queue",
+	parameters = {
+		@Parameter(name = "index", description = "The index of the song to remove.", type = OptionType.INTEGER, required = true)					
+	},
 	aliases = {"removesong"},
 	category = CommandCategory.Audio
 )
