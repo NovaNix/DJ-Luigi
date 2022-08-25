aa9c1067ce2d7af37f036b24e34833799e850f5c
Michael Nix <michael.g.nix@gmail.com>
2022-08-24 21:08:07 -0500

    Added the option to set the loop state

--- src/DJLuigi/Commands/Audio/LoopCommand.java
+++ src/DJLuigi/Commands/Audio/LoopCommand.java
@@ -3,13 +3,18 @@ package DJLuigi.Commands.Audio;
 import DJLuigi.Commands.Command;
 import DJLuigi.Commands.CommandCategory;
 import DJLuigi.Commands.CommandData;
+import DJLuigi.Commands.Parameter;
 import DJLuigi.Server.Server;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 
 @CommandData
 (
 	command = "loop", 
-	description = "Toggles if the queue should be looped",
+	description = "Sets if the queue should be looped or not",
+	parameters = {
+		@Parameter(name = "looped", description = "The new looped status", type = OptionType.BOOLEAN)
+	},
 	aliases = {"l"},
 	djOnly = true,
 	category = CommandCategory.Audio
@@ -20,7 +25,18 @@ public class LoopCommand extends Command
 	@Override
 	public void executeCommand(Server S, SlashCommandInteractionEvent event) 
 	{
-		event.reply("Set looped status to : `" + S.queue.toggleLoop() + "`!").queue();
+		if (event.getOption("looped") == null)
+		{
+			event.reply("Set looped status to : `" + S.queue.toggleLoop() + "`!").queue();
+		}
+		
+		else
+		{
+			boolean loopStatus = event.getOption("looped").getAsBoolean();
+			S.queue.setLoop(loopStatus);
+			event.reply("Set looped status to : `" + loopStatus + "`!").queue();
+		}
+		
 	}
 
 }
